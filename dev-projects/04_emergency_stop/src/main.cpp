9036a09742888e67c388c9abea6529c0cbd4c56d
kyrins <192162104+Kyr1ns@users.noreply.github.com>
2026-06-20 22:35:09 +0700

    Revert "nambah kode emergency stop"
    This reverts commit 0ab235c035f555397ab2c4045482e7bd87c84b4d.

--- dev-projects/04_emergency_stop/src/main.cpp
+++ dev-projects/04_emergency_stop/src/main.cpp
@@ -1,56 +1,18 @@
 #include <Arduino.h>
 
-#include "EmergencyStop.h"
+// put function declarations here:
+int myFunction(int, int);
 
-EmergencyStop::Config safetyConfig
-{
-    .mq2Pin = 34,
-
-    .estopPin = 25,
-
-    .feedbackPin = 26,
-
-    .mq2Threshold = 500,
-
-    .debounceMs = 50,
-
-    .estopActiveLow = true,
-
-    .feedbackActiveLow = true
-};
-
-EmergencyStop safety(
-    safetyConfig
-);
-
-void setup()
-{
-    Serial.begin(115200);
-
-    safety.begin();
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
 }
 
-void loop()
-{
-    safety.update();
+void loop() {
+  // put your main code here, to run repeatedly:
+}
 
-    if(Serial.available())
-    {
-        String cmd =
-        Serial.readStringUntil('\n');
-
-        cmd.trim();
-
-        if(cmd == "debug")
-        {
-            safety.printDebug(
-                Serial
-            );
-        }
-
-        if(cmd == "reset")
-        {
-            safety.resetEmergency();
-        }
-    }
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
