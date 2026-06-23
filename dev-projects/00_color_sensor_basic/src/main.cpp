9bc701627815b4ce40fc0d5935ead0613e56ffae
arrvy <nashr.wahyono@mhs.unsoed.ac.id>
2026-06-24 06:50:52 +0700

    Add initial project structure and implement RGB sensor calibration with Modbus support

---
+++ dev-projects/00_color_sensor_basic/src/main.cpp
@@ -0,0 +1,236 @@
+#include <Arduino.h>
+
+constexpr uint8_t PIN_LED_R = 33;
+constexpr uint8_t PIN_LED_G = 25;
+constexpr uint8_t PIN_LED_B = 26;
+
+constexpr uint8_t PIN_PHOTODIODE = 34;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(PIN_LED_R, OUTPUT);
+    pinMode(PIN_LED_G, OUTPUT);
+    pinMode(PIN_LED_B, OUTPUT);
+
+    digitalWrite(PIN_LED_R, LOW);
+    digitalWrite(PIN_LED_G, LOW);
+    digitalWrite(PIN_LED_B, LOW);
+
+    analogReadResolution(12);
+
+    Serial.println();
+    Serial.println("=================================");
+    Serial.println(" RGB SENSOR CALIBRATION MODE");
+    Serial.println("=================================");
+}
+
+void loop()
+{
+    // uint16_t ambient;
+    // uint16_t redRaw;
+    // uint16_t greenRaw;
+    // uint16_t blueRaw;
+
+    // //----------------------------------
+    // // Ambient
+    // //----------------------------------
+
+    // Serial.println();
+    // Serial.println("Reading Ambient...");
+
+    // digitalWrite(PIN_LED_R, LOW);
+    // digitalWrite(PIN_LED_G, LOW);
+    // digitalWrite(PIN_LED_B, LOW);
+
+    // delay(1000);
+
+    // ambient = analogRead(PIN_PHOTODIODE);
+
+    // //----------------------------------
+    // // RED
+    // //----------------------------------
+
+    // Serial.println("Reading RED...");
+
+    // digitalWrite(PIN_LED_R, HIGH);
+
+    // delay(1000);
+
+    // redRaw = analogRead(PIN_PHOTODIODE);
+
+    // digitalWrite(PIN_LED_R, LOW);
+
+    // //----------------------------------
+    // // GREEN
+    // //----------------------------------
+
+    // Serial.println("Reading GREEN...");
+
+    // digitalWrite(PIN_LED_G, HIGH);
+
+    // delay(1000);
+
+    // greenRaw = analogRead(PIN_PHOTODIODE);
+
+    // digitalWrite(PIN_LED_G, LOW);
+
+    // //----------------------------------
+    // // BLUE
+    // //----------------------------------
+
+    // Serial.println("Reading BLUE...");
+
+    // digitalWrite(PIN_LED_B, HIGH);
+
+    // delay(1000);
+
+    // blueRaw = analogRead(PIN_PHOTODIODE);
+
+    // digitalWrite(PIN_LED_B, LOW);
+
+    // //----------------------------------
+    // // Ambient Compensation
+    // //----------------------------------
+
+    // int redComp =
+    //     max(0, (int)redRaw - (int)ambient);
+
+    // int greenComp =
+    //     max(0, (int)greenRaw - (int)ambient);
+
+    // int blueComp =
+    //     max(0, (int)blueRaw - (int)ambient);
+
+    // //----------------------------------
+    // // RAW DATA
+    // //----------------------------------
+
+    // Serial.println();
+    // Serial.println("===== RAW ADC =====");
+
+    // Serial.print("Ambient   : ");
+    // Serial.println(ambient);
+
+    // Serial.print("Red Raw   : ");
+    // Serial.println(redRaw);
+
+    // Serial.print("Green Raw : ");
+    // Serial.println(greenRaw);
+
+    // Serial.print("Blue Raw  : ");
+    // Serial.println(blueRaw);
+
+    // //----------------------------------
+    // // COMPENSATED DATA
+    // //----------------------------------
+
+    // Serial.println();
+    // Serial.println("===== COMPENSATED ADC =====");
+
+    // Serial.print("Red   : ");
+    // Serial.println(redComp);
+
+    // Serial.print("Green : ");
+    // Serial.println(greenComp);
+
+    // Serial.print("Blue  : ");
+    // Serial.println(blueComp);
+
+    // //----------------------------------
+    // // NORMALIZATION
+    // //----------------------------------
+
+    // float total =
+    //     redComp +
+    //     greenComp +
+    //     blueComp;
+
+    // Serial.println();
+    // Serial.println("===== NORMALIZED RGB =====");
+
+    // if (total > 0.0f)
+    // {
+    //     float r = redComp / total;
+    //     float g = greenComp / total;
+    //     float b = blueComp / total;
+
+    //     Serial.print("R : ");
+    //     Serial.print(r, 4);
+
+    //     Serial.print(" | G : ");
+    //     Serial.print(g, 4);
+
+    //     Serial.print(" | B : ");
+    //     Serial.println(b, 4);
+    // }
+    // else
+    // {
+    //     Serial.println("No Valid Signal");
+    // }
+
+    // Serial.println();
+    // Serial.println("==============================");
+    // Serial.println("Next Sample In 2 Seconds...");
+    // Serial.println("==============================");
+
+    // delay(2000);
+
+     uint16_t redRaw;
+    uint16_t greenRaw;
+    uint16_t blueRaw;
+
+    // RED
+    digitalWrite(PIN_LED_R, HIGH);
+    delay(1000);
+
+    redRaw = analogRead(PIN_PHOTODIODE);
+
+    digitalWrite(PIN_LED_R, LOW);
+
+    // GREEN
+    digitalWrite(PIN_LED_G, HIGH);
+    delay(1000);
+
+    greenRaw = analogRead(PIN_PHOTODIODE);
+
+    digitalWrite(PIN_LED_G, LOW);
+
+    // BLUE
+    digitalWrite(PIN_LED_B, HIGH);
+    delay(1000);
+
+    blueRaw = analogRead(PIN_PHOTODIODE);
+
+    digitalWrite(PIN_LED_B, LOW);
+
+    //--------------------------------
+    // DISPLAY
+    //--------------------------------
+
+    Serial.println();
+    Serial.println("--------------------------------");
+
+    Serial.print("R : ");
+    Serial.println(redRaw);
+
+    Serial.print("G : ");
+    Serial.println(greenRaw);
+
+    Serial.print("B : ");
+    Serial.println(blueRaw);
+
+    Serial.print("RGB = [");
+    Serial.print(redRaw);
+    Serial.print(", ");
+    Serial.print(greenRaw);
+    Serial.print(", ");
+    Serial.print(blueRaw);
+    Serial.println("]");
+
+    Serial.println("--------------------------------");
+
+    delay(2000);
+ 
+}
--- dev-projects/06.2_iot_modbus/src/main.cpp
+++ dev-projects/06.2_iot_modbus/src/main.cpp
@@ -1,18 +1,64 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ModbusTCP.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "KonSer.net";
+const char* password = "servocaporangtua45";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ModbusTCP mb;
+
+IPAddress local_IP(192,168,18,60);
+IPAddress gateway(192,168,18,1);
+IPAddress subnet(255,255,255,0);
+
+#define LED_PIN 2
+
+uint16_t counter = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(LED_PIN, OUTPUT);
+
+    WiFi.config(local_IP, gateway, subnet);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.print("IP : ");
+    Serial.println(WiFi.localIP());
+
+    mb.server();
+
+    // Holding Registers
+    mb.addHreg(0);
+    mb.addHreg(1);    
+
+    // Coil
+    mb.addCoil(0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    mb.task();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    counter++;
+
+    mb.Hreg(0, counter);
+
+    int adc = analogRead(34);
+    mb.Hreg(1, adc);
+
+    bool ledState = mb.Coil(0);
+
+    digitalWrite(LED_PIN, ledState);
+
+    delay(100);
 }
