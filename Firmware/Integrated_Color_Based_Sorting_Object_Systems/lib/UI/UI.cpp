06783225068092bd5618cdb6f9747ee0afa91300
arrvy <nashr.wahyono@mhs.unsoed.ac.id>
2026-06-29 01:57:59 +0700

    refactor: remove overloaded setSystemState method to simplify state management

--- Firmware/Integrated_Color_Based_Sorting_Object_Systems/lib/UI/UI.cpp
+++ Firmware/Integrated_Color_Based_Sorting_Object_Systems/lib/UI/UI.cpp
@@ -53,11 +53,6 @@ void UI::update()
     updateLCD();
 }
 
-void UI::setSystemState(SystemState state)
-{
-    _stateText = stateToString(state);
-}
-
 void UI::setSystemState(const String& stateText)
 {
     _stateText = stateText;
